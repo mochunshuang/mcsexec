9f9a615a069eedb7719340e9d5d98ab58e9f551a
mochunshuang <1651798639@qq.com>
2024-12-22 22:52:23 +0800

    product_type 增加Allocator，且apply不需要forward_like了 自带this显示了

--- include/__detail/snd/__detail/__product_type.hpp
+++ include/__detail/snd/__detail/__product_type.hpp
@@ -2,14 +2,15 @@
 
 #include <tuple>
 #include <utility>
+#include <memory>
 
 namespace mcs::execution::snd::__detail
 {
-
     template <::std::size_t, typename T>
     struct product_type_element
     {
         T value; // NOLINT
+        auto operator==(const product_type_element &) const -> bool = default;
     };
 
     template <typename, typename...>
@@ -17,7 +18,7 @@ namespace mcs::execution::snd::__detail
 
     template <::std::size_t... I, typename... T>
     struct product_type_base<::std::index_sequence<I...>, T...>
-        : product_type_element<I, T>...
+        : ::mcs::execution::snd::__detail::product_type_element<I, T>...
     {
         static constexpr ::std::size_t size()
         {
@@ -26,19 +27,22 @@ namespace mcs::execution::snd::__detail
 
         template <::std::size_t J, typename S>
         static auto element_get( // NOLINT
-            product_type_element<J, S> &self) noexcept -> S &
+            ::mcs::execution::snd::__detail::product_type_element<J, S> &self) noexcept
+            -> S &
         {
             return self.value;
         }
         template <::std::size_t J, typename S>
         static auto element_get( // NOLINT
-            product_type_element<J, S> &&self) noexcept -> S
+            ::mcs::execution::snd::__detail::product_type_element<J, S> &&self) noexcept
+            -> S &&
         {
             return ::std::move(self.value);
         }
         template <::std::size_t J, typename S>
         static auto element_get( // NOLINT
-            product_type_element<J, S> const &self) noexcept -> S const &
+            const ::mcs::execution::snd::__detail::product_type_element<J, S>
+                &self) noexcept -> const S &
         {
             return self.value;
         }
@@ -49,7 +53,7 @@ namespace mcs::execution::snd::__detail
             return this->element_get<J>(*this);
         }
         template <::std::size_t J>
-        auto get() && -> auto
+        auto get() && -> decltype(auto)
         {
             return this->element_get<J>(::std::move(*this));
         }
@@ -59,27 +63,80 @@ namespace mcs::execution::snd::__detail
             return this->element_get<J>(*this);
         }
 
-        auto operator==(product_type_base const &) const -> bool = default;
+        template <::std::size_t J, typename Allocator, typename Self> // NOLINTNEXTLINE
+        static auto make_element(Allocator &&alloc, Self &&self) -> decltype(auto)
+        {
+            using type = ::std::remove_cvref_t<decltype(product_type_base::element_get<J>(
+                std::forward<Self>(self)))>;
+            if constexpr (::std::uses_allocator_v<type, Allocator>)
+                return ::std::make_obj_using_allocator<type>(
+                    alloc, product_type_base::element_get<J>(std::forward<Self>(self)));
+            else
+                return product_type_base::element_get<J>(std::forward<Self>(self));
+        }
+
+        auto operator==(const product_type_base &) const -> bool = default;
     };
 
     template <typename... T>
-    struct product_type : product_type_base<::std::index_sequence_for<T...>, T...>
+    struct product_type : ::mcs::execution::snd::__detail::product_type_base<
+                              ::std::index_sequence_for<T...>, T...>
     {
-        template <class Self, class Fn>
-            requires ::std::invocable<Fn, T &...>
-        constexpr decltype(auto) apply(this Self &&self, Fn &&fn) noexcept(
-            ::std::is_nothrow_invocable_v<Fn, T &...>)
+
+        template <typename Allocator, typename Product, std::size_t... I>
+        static auto make_from(Allocator &&allocator, Product &&product, // NOLINT
+                              std::index_sequence<I...> /*unused*/) -> product_type
         {
-            return
-                [&]<::std::size_t... I>(::std::index_sequence<I...>) -> decltype(auto) {
-                    return ::std::forward<Fn>(fn)(
-                        ::std::forward_like<Self>(self.template get<I>())...);
-                }(::std::index_sequence_for<T...>{});
+            return {product_type::template make_element<I>(
+                allocator, ::std::forward<Product>(product))...};
+        }
+
+        template <typename Allocator, typename Product> // NOLINTNEXTLINE
+        static auto make_from(Allocator &&allocator, Product &&product) -> product_type
+        {
+            return product_type::make_from(::std::forward<Allocator>(allocator),
+                                           ::std::forward<Product>(product),
+                                           ::std::index_sequence_for<T...>{});
+        }
+
+        template <typename Fun, ::std::size_t... I> // NOLINTNEXTLINE
+        constexpr auto apply_elements(::std::index_sequence<I...>,
+                                      Fun &&fun) const -> decltype(auto)
+        {
+            return ::std::forward<Fun>(fun)(this->template get<I>()...);
+        }
+        template <typename Fun>
+        constexpr auto apply(Fun &&fun) const -> decltype(auto)
+        {
+            return apply_elements(::std::index_sequence_for<T...>{},
+                                  ::std::forward<Fun>(fun));
+        }
+        template <typename Fun, ::std::size_t... I> // NOLINTNEXTLINE
+        constexpr auto apply_elements(::std::index_sequence<I...>,
+                                      Fun &&fun) -> decltype(auto)
+        {
+            return ::std::forward<Fun>(fun)(this->template get<I>()...);
+        }
+        template <typename Fun>
+        constexpr auto apply(Fun &&fun) -> decltype(auto)
+        {
+            return apply_elements(::std::index_sequence_for<T...>{},
+                                  ::std::forward<Fun>(fun));
         }
     };
-
     template <typename... T>
     product_type(T &&...) -> product_type<::std::decay_t<T>...>;
+
+    // helper
+    template <typename T>
+    constexpr bool is_product_type_v = false; // NOLINT
+
+    template <typename... U>
+    constexpr bool // NOLINTNEXTLINE
+        is_product_type_v<::mcs::execution::snd::__detail::product_type<U...>> = true;
+
+    template <typename T>
+    concept is_product_type = is_product_type_v<T>;
 
 }; // namespace mcs::execution::snd::__detail
 
@@ -98,4 +155,16 @@ namespace std
             decltype(::std::declval<::mcs::execution::snd::__detail::product_type<T...>>()
                          .template get<I>())>;
     };
+}; // namespace std
+
+namespace std
+{
+    // for std::get<>
+    template <std::size_t I, typename T>
+        requires ::mcs::execution::snd::__detail::is_product_type<
+                     ::std::remove_cvref_t<T>>
+    constexpr auto get(T &&t) noexcept -> decltype(auto) // NOLINT
+    {
+        return std::forward<T>(t).template get<I>();
+    }
 }; // namespace std
