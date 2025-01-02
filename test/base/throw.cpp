ef7280c780ec68c0317b8c29d07b21296150dd86
mochunshuang <1651798639@qq.com>
2025-01-02 15:34:22 +0800

    std::exception_ptr 可以适配所有异常

--- test/base/throw.cpp
+++ test/base/throw.cpp
@@ -1,4 +1,5 @@
 
+#include <cassert>
 #include <exception>
 #include <stdexcept>
 #include <type_traits>
@@ -13,6 +14,7 @@ auto fun0() -> int // NOLINT
 auto fun1() {}          // NOLINT
 auto fun2() noexcept {} // NOLINT
 
+auto catch_throw(); // NOLINT
 int main()
 {
 
@@ -26,4 +28,37 @@ int main()
     using T = decltype(std::current_exception());
     static_assert(std::is_same_v<std::exception_ptr, T>);
     return 0;
+}
+
+auto catch_throw()
+{
+    auto fun = [](std::exception_ptr &&e) {
+        // 如何拿到前面的 std::logic_error("error");的信息
+        try
+        {
+            std::rethrow_exception(e); // 重新抛出异常
+        }
+        catch (const std::logic_error &ex) // 专门捕获 std::logic_error
+        {
+            assert(std::string(ex.what()) == "err");
+        }
+        catch (const std::exception &ex) // 捕获其他 std::exception 派生类
+        {
+            assert(false);
+        }
+        catch (...) // 捕获所有其他异常
+        {
+            assert(false);
+        }
+        // Note: 返回值只能是一个类型 。 std::exception_ptr 是很好的抽象
+    };
+    try
+    {
+        [[maybe_unused]] int ret = fun0();
+        assert(false);
+    }
+    catch (...)
+    {
+        fun(std::current_exception());
+    }
 }
