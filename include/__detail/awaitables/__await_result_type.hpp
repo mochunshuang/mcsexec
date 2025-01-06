1b123e9381720356324168d851439bf0497f5415
mochunshuang <1651798639@qq.com>
2025-01-07 00:17:15 +0800

    GET_AWAITER要求左值,修正模板推导

--- include/__detail/awaitables/__await_result_type.hpp
+++ include/__detail/awaitables/__await_result_type.hpp
@@ -7,7 +7,7 @@ namespace mcs::execution::awaitables
     // await-result-type
     template <class C, class Promise>
     using await_result_type = decltype(awaitables::__detail::GET_AWAITER(
-                                           std::declval<C>(), std::declval<Promise>())
+                                           std::declval<C>(), std::declval<Promise &>())
                                            .await_resume());
 
 }; // namespace mcs::execution::awaitables
