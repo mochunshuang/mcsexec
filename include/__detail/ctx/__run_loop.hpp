c535073d596d1013f7282c2b888fa69b98ea3593
mochunshuang <1651798639@qq.com>
2025-01-30 17:24:35 +0800

    简化模板能力

--- include/__detail/ctx/__run_loop.hpp
+++ include/__detail/ctx/__run_loop.hpp
@@ -164,8 +164,8 @@ namespace mcs::execution::ctx
                 template <typename Self, typename Rcvr>
                 auto connect(this Self &&self,
                              Rcvr rcvr) noexcept(noexcept((void(self), auto(rcvr))))
-                    -> run_loop_opstate<std::decay_t<decltype((rcvr))>>
-                    requires(recv::receiver_of<decltype((rcvr)), completion_signatures>)
+                    -> run_loop_opstate<std::decay_t<Rcvr>>
+                    requires(recv::receiver_of<decltype(rcvr), completion_signatures>)
                 {
                     return run_loop_opstate{std::forward<Self>(self).run_loop_,
                                             std::move(rcvr)};
