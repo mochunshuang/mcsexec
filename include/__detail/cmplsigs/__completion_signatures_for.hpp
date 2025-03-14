1e3314f439c3084b748af08a22c0d30e66fc20eb
mochunshuang <1651798639@qq.com>
2025-03-14 17:19:12 +0800

    签名类型重构

--- include/__detail/cmplsigs/__completion_signatures_for.hpp
+++ include/__detail/cmplsigs/__completion_signatures_for.hpp
@@ -7,7 +7,7 @@ namespace mcs::execution::cmplsigs
     {
     };
 
-    template <class Sndr, class Env>
+    template <class Sndr, class... Env>
     struct completion_signatures_for_impl;
 
     namespace __detail
@@ -15,27 +15,28 @@ namespace mcs::execution::cmplsigs
         template <template <class...> class T, class... Args>
         concept has_completion_type = requires { typename T<Args...>::type; };
 
-        template <class Sndr, class Env>
+        template <class Sndr, class... Env>
         struct __completion_signatures_for;
 
-        template <class Sndr, class Env>
-            requires(not has_completion_type<completion_signatures_for_impl, Sndr, Env>)
-        struct __completion_signatures_for<Sndr, Env>
+        template <class Sndr, class... Env>
+            requires(
+                not has_completion_type<completion_signatures_for_impl, Sndr, Env...>)
+        struct __completion_signatures_for<Sndr, Env...>
         {
             using type = undefine_completion_signatures_for;
         };
 
-        template <class Sndr, class Env>
-            requires(has_completion_type<completion_signatures_for_impl, Sndr, Env>)
-        struct __completion_signatures_for<Sndr, Env>
+        template <class Sndr, class... Env>
+            requires(has_completion_type<completion_signatures_for_impl, Sndr, Env...>)
+        struct __completion_signatures_for<Sndr, Env...>
         {
-            using type = typename completion_signatures_for_impl<Sndr, Env>::type;
+            using type = typename completion_signatures_for_impl<Sndr, Env...>::type;
         };
 
     }; // namespace __detail
 
-    template <class Sndr, class Env>
+    template <class Sndr, class... Env>
     using completion_signatures_for = // exposition only
-        typename __detail::__completion_signatures_for<Sndr, Env>::type;
+        typename __detail::__completion_signatures_for<Sndr, Env...>::type;
 
 }; // namespace mcs::execution::cmplsigs
