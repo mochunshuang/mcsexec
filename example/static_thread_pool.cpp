f1db0150daa9578ebeccdf04d627ac48e3ac69f2
mochunshuang <1651798639@qq.com>
2025-09-10 22:14:38 +0800

    fix hightline example

--- example/static_thread_pool.cpp
+++ example/static_thread_pool.cpp
@@ -5,13 +5,13 @@
 int main()
 {
     using namespace mcs::execution;
-    for (int i = 0; i < 100000; i++)
+    for (int i = 0; i < 10000; i++)
     {
         static_thread_pool<3> thread_pool;
         scheduler auto sched6 = thread_pool.get_scheduler();
         auto s7 = schedule(sched6) | then([] {});
 
-        if (i > 0 && i % 10000 == 0)
+        if (i > 0 && i % 1000 == 0)
             std::cout << "i: " << i << " thread_pool  done\n";
         mcs::this_thread::sync_wait(s7);
     }
