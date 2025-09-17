53c1b0ad202d277ac040ca270bc22a1c7e5fcd3f
mochunshuang <1651798639@qq.com>
2025-09-17 16:02:35 +0800

    task 删除 lazy 代码

--- include/__detail/__task.hpp
+++ include/__detail/__task.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-#include "./task/__lazy.hpp"
 #include "./task/__task.hpp"
 
 namespace mcs::execution
 {
 
-    using ::mcs::execution::__task::lazy;
     using ::mcs::execution::__task::task;
 
 }; // namespace mcs::execution
