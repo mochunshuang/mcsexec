b313487ce803039e4c2607cd12d0977a6988dfa2
mochunshuang <1651798639@qq.com>
2025-10-30 15:07:05 +0800

    remove v2 to test.

--- test/task/test_task_v2.cpp
+++ test/task/test_task_v2.cpp
@@ -2,11 +2,11 @@
 #include <exception>
 #include <iostream>
 #include <thread>
-#include "task_v2/__task.hpp"
+// #include "task_v2/__task.hpp"
 
 int main()
 {
-    namespace ex = ::mcs::execution;
+    // namespace ex = ::mcs::execution;
 
     std::cout << "main done\n";
     return 0;
